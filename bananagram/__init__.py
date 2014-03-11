9908127412d7e8d465bd09674d53070baa30f5d3
Katherine Elliott <katherine@radiantradon.com>
2014-03-11 04:27:02 -0700

    Update __init__.py
    Updating __init__.py to go from nonsensical 100,000 "word" dictionary to a smaller 1000 word dictionary.

--- bananagram/__init__.py
+++ bananagram/__init__.py
@@ -8,5 +8,5 @@ from node import Node
 # We do this in __init__.py so that the word list will only have to regenerate
 # when the server restarts
 trie = Node()
-trie.fill(filename="dictionary.txt")
+trie.fill(filename="1000.txt")
 
